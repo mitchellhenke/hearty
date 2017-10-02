e77117c6cefa66c97cf167d1a39bbaea7b165907
Nick Gartmann <nick.gartmann@gmail.com>
2017-10-02 16:04:05 -0500

    Move the query work over to the request process

--- lib/hearty/shapefiles/server.ex
+++ lib/hearty/shapefiles/server.ex
@@ -13,37 +13,11 @@ defmodule Hearty.Shapefiles.Server do
     GenServer.start_link(__MODULE__, zipped, name: __MODULE__)
   end
 
-  def query(latitude, longitude) do
-    GenServer.call(__MODULE__, {:query, latitude, longitude})
+  def get() do
+    GenServer.call(__MODULE__, :get)
   end
 
-  def handle_call({:query, latitude, longitude}, _, state) do
-    result = state
-      |> Enum.filter(fn({shape, _dbf}) -> 
-        Map.has_key?(shape, :points)
-      end)
-      |> Enum.map(fn({shape, dbf}) -> 
-        {
-          shape,
-          %{type: "Polygon", coordinates: [Enum.map(hd(shape.points), fn(%{x: x, y: y}) -> {x,y} end)]},
-          %{type: "Polygon", coordinates: [[{shape.bbox.xmin,shape.bbox.ymin},{shape.bbox.xmin,shape.bbox.ymax},{shape.bbox.xmax,shape.bbox.ymax}, {shape.bbox.xmax,shape.bbox.ymin}]]},
-          dbf
-        }
-      end)
-      |> Enum.filter(fn({_shape, _, bbox,  _dbf}) ->
-        Topo.contains?(bbox, %{type: "Point", coordinates: {latitude, longitude}})
-      end)
-      |> Enum.filter(fn({_shape, polygon, _bbox, _dbf}) ->
-        Topo.contains?(polygon, %{type: "Point", coordinates: {latitude, longitude}})
-      end)
-
-    case result do 
-      [{_, _, _, [_, zone]} | _] -> {:reply, zone, state}
-      _ -> {:reply, nil, state}
-    end
-    
-  end
-
+  def handle_call(:get, _, state), do: {:reply, state, state}
 end
 
 
--- lib/hearty/shapefiles/shapefiles.ex
+++ lib/hearty/shapefiles/shapefiles.ex
@@ -10,6 +10,30 @@ defmodule Hearty.Shapefiles do
     )
   end
 
-  def query(lat, lng), do: Shapefiles.Server.query(lat, lng)
+  def query(latitude, longitude) do
+    Shapefiles.Server.get()
+    |> Enum.filter(fn({shape, _dbf}) -> 
+      Map.has_key?(shape, :points)
+    end)
+    |> Enum.map(fn({shape, dbf}) -> 
+      {
+        shape,
+        %{type: "Polygon", coordinates: [Enum.map(hd(shape.points), fn(%{x: x, y: y}) -> {x,y} end)]},
+        %{type: "Polygon", coordinates: [[{shape.bbox.xmin,shape.bbox.ymin},{shape.bbox.xmin,shape.bbox.ymax},{shape.bbox.xmax,shape.bbox.ymax}, {shape.bbox.xmax,shape.bbox.ymin}]]},
+        dbf
+      }
+    end)
+    |> Enum.filter(fn({_shape, _, bbox,  _dbf}) ->
+      Topo.contains?(bbox, %{type: "Point", coordinates: {latitude, longitude}})
+    end)
+    |> Enum.filter(fn({_shape, polygon, _bbox, _dbf}) ->
+      Topo.contains?(polygon, %{type: "Point", coordinates: {latitude, longitude}})
+    end)
+    |> case do 
+      [{_, _, _, [_, zone]} | _] -> zone
+      _ -> nil
+    end
+
+  end
 
 end
